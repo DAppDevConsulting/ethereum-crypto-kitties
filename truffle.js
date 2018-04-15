7c59272c020428e64921e9aecd8f23d664a9346b
Dmytrii Kulbaka <kulbaka.dmytrii@gmail.com>
2018-04-15 16:31:53 +0300

    updated truffle.js config

--- truffle.js
+++ truffle.js
@@ -1,15 +1,11 @@
-let HDWalletProvider = require('truffle-hdwallet-provider');
-
-let mnemonic = "citizen spread heart traffic accuse umbrella analyst afford donor remove shine agent";
-
 module.exports = {
   networks: {
     development: {
       // provider: HDWalletProvider(mnemonic, "http://localhost:8545"),
       host: 'localhost',
-      port: 8545,
+      port: 7545,
       network_id: 5777,
-      gas: 19000000,
+      gas: 4000000,
     }
   }
 };
